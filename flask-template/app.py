ce1492d44c9f9823380de0743e77fe0220903e64
Mekeih J.C. Nelson <mekeih@techexchange.in>
2022-03-21 18:16:57 +0000

    Finished quiz

--- flask-template/app.py
+++ flask-template/app.py
@@ -15,21 +15,28 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
 
 
 # -- Initialization section --
 app = Flask(__name__)
 
-
+# Mekeih Nelson and Teshi
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
 
-@app.route('/captials')
-def capitals():
-    
-    return render_template('capital.hmtl', capitals = capitals)
+@app.route('/results', methods=['GET','POST'])
+def results():
+    anwsers = {
+    'NY_Capital':[request.form["NY Capital"],'Albany'],
+    'CA_Capital':[request.form["CA Capital"],'Sacramento'],
+    'MD_Capital':[request.form["MD Capital"],'Annapolis'],
+    'AL_Capital':[request.form["AL Capital"],'Juneau'],
+    'HI_Capital':[request.form["HI Capital"],'Honolulu']
+    }
+
+    return render_template("results.html", eval=anwsers)
